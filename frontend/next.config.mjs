7d539305574fba2850d8fcad37e5757abcc4ec1a
amansharma-bit <aman.sharma@visa2fly.com>
2026-05-12 12:05:50 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'photos.hotelbeds.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 }
